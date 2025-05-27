41dec4492ea003f564d8239a4d3a300e90ce614c
Aleksandr Bannikov <alexdinaro@gmail.com>
2025-05-27 11:13:38 +0200

    Add more configure functions to FlowBuilder

--- src/libraries/FlowBuilder.sol
+++ src/libraries/FlowBuilder.sol
@@ -13,14 +13,16 @@ library FlowBuilder {
      * @dev Struct holding configuration for a flow to be created.
      */
     struct FlowConfig {
+        address oraclePool;
+        address flowRegistry;
+
         HTTPRequest request;
         HTTPPrivatePatch patch;
         string jqFilter;
         string responseSchema;
+
         address consumer;
         bytes4 callback;
-        address oraclePool;
-        address flowRegistry;
         uint256 gasLimit;
     }
 
@@ -86,6 +88,126 @@ library FlowBuilder {
     {
         config.consumer = consumer;
         config.callback = callback;
+        return config;
+    }
+
+    /**
+     * @notice Sets the method for the HTTP request.
+     * @param config Existing FlowConfig.
+     * @param method Method to set.
+     * @return Updated FlowConfig.
+     */
+    function withMethod(FlowConfig memory config, RequestMethod method)
+        internal
+        pure
+        returns (FlowConfig memory)
+    {
+        config.request.method = method;
+        return config;
+    }
+
+    /**
+     * @notice Sets the headers for the HTTP request.
+     * @param config Existing FlowConfig.
+     * @param headers Headers to set.
+     * @return Updated FlowConfig.
+     */
+    function withHeaders(FlowConfig memory config, RequestHeader[] memory headers)
+        internal
+        pure
+        returns (FlowConfig memory)
+    {
+        config.request.headers = headers;
+        return config;
+    }
+
+    /**
+     * @notice Sets the query parameters for the HTTP request.
+     * @param config Existing FlowConfig.
+     * @param parameters Query parameters to set.
+     * @return Updated FlowConfig.
+     */
+    function withQueryParameters(FlowConfig memory config, QueryParameter[] memory parameters)
+        internal
+        pure
+        returns (FlowConfig memory)
+    {
+        config.request.parameters = parameters;
+        return config;
+    }
+
+    /**
+     * @notice Sets the body for the HTTP request.
+     * @param config Existing FlowConfig.
+     * @param body Body to set.
+     * @return Updated FlowConfig.
+     */
+    function withBody(FlowConfig memory config, bytes memory body)
+        internal
+        pure
+        returns (FlowConfig memory)
+    {
+        config.request.body = body;
+        return config;
+    }
+
+    /**
+     * @notice Sets the private path suffix for the HTTP request.
+     * @param config Existing FlowConfig.
+     * @param pathSuffix Private path suffix to set.
+     * @return Updated FlowConfig.
+     */
+    function withPrivatePathSuffix(FlowConfig memory config, bytes memory pathSuffix)
+        internal
+        pure
+        returns (FlowConfig memory)
+    {
+        config.patch.pathSuffix = pathSuffix;
+        return config;
+    }
+
+    /**
+     * @notice Sets the private headers for the HTTP request.
+     * @param config Existing FlowConfig.
+     * @param headers Private headers to set.
+     * @return Updated FlowConfig.
+     */
+    function withPrivateHeaders(FlowConfig memory config, RequestHeaderPatch[] memory headers)
+        internal
+        pure
+        returns (FlowConfig memory)
+    {
+        config.patch.headers = headers;
+        return config;
+    }
+
+    /**
+     * @notice Sets the private query parameters for the HTTP request.
+     * @param config Existing FlowConfig.
+     * @param parameters Private query parameters to set.
+     * @return Updated FlowConfig.
+     */
+    function withPrivateQueryParameters(FlowConfig memory config, QueryParameterPatch[] memory parameters)
+        internal
+        pure
+        returns (FlowConfig memory)
+    {
+        config.patch.parameters = parameters;
+        return config;
+    }
+
+    /**
+     * @notice Sets the private body for the HTTP request.
+     * @param config Existing FlowConfig.
+     * @param body Private body to set.
+     * @return Updated FlowConfig.
+     */
+    function withPrivateBody(FlowConfig memory config, bytes memory body)
+        internal
+        pure
+        returns (FlowConfig memory)
+    {
+        config.patch.body = body;
         return config;
     }
 
