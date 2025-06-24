f0454d3f873b520ac968bc1aa57c63a125e5ab64
catena <catena@protonmail.com>
2025-06-24 17:15:52 +0300

    update interfaces to current implementation

---
+++ src/interfaces/core/IP256Verifier.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.22;
+
+interface IP256Verifier {
+    function ecdsa_verify(
+        bytes32 message_hash,
+        uint256 r,
+        uint256 s,
+        uint256[2] memory pubKey
+    ) external view returns (bool);
+}
